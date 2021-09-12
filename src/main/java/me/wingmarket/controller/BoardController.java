be5e84f6409c6884d796a7282a1a2312acd848e1
sujl95 <sujl95@naver.com>
2021-09-12 23:43:04 +0900

    Fix log package name

--- src/main/java/me/wingmarket/controller/BoardController.java
+++ src/main/java/me/wingmarket/controller/BoardController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.SessionAttribute;
@@ -81,4 +82,5 @@ public class BoardController {
 	public void deleteBoard(@PathVariable Long id, @SessionAttribute("ID") Long userId) {
 		boardService.deleteById(id, userId);
 	}
+
 }
